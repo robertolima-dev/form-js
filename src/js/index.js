a87c58d2ac9ba59337308c7c4e1355f335c88ea5
Marcus Vinicius <webdevcampos@gmail.com>
2022-02-15 11:52:13 -0300

    testing fetch API

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,22 @@
 const form = document.querySelector('form');
+const main = document.querySelector('main')
 const userName = document.querySelector('[name="nome"]');
 const userEmail = document.querySelector('[name="email"]');
 const userPhone = document.querySelector('[name="telefone"]');
 const userPassword = document.querySelector('[name="senha"]');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (userName.value && userEmail.value && userPhone.value && userPassword.value) {
+  const formIsFulfilled = userName.value && userEmail.value && userPhone.value && userPassword.value
+  if (formIsFulfilled) {
     fetch('http://localhost:5500/form-js/cadastrou.json').then((res) => {
       const answer = res.json();
       return answer
     }).then(answer => {
       console.log(answer)
-      document.body.innerHTML = `<p>${answer.success}</p>`
+      main.innerHTML = `<p>${answer.success}</p>`
     })
   } else {
-    document.body.innerHTML = `<p>Não</p>`
+    main.innerHTML = `<p>Não</p>`
   }
 
 })
